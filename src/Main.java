c709c29d3d74cc84988c4f2aa8284f126c307d9f
RodrigoReis-dev <89714207+RodrigoReis-dev@users.noreply.github.com>
2025-06-25 00:06:35 +1200

    Initial commit - added Main.java with basic print statements Added print statement on feature-branch-1

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
 		System.out.println("Task 9: Source and Version Control.");
+		System.out.println("This is a change on feature-branch-1.");
 	}
 }
-
-git config user.name "Caio Reis"
-git config user.email "caioreis@myop.ac.nz"
