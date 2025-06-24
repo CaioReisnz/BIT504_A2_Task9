d18e24c8fdeac32011141faac639dd3d45d91194
RodrigoReis-dev <89714207+RodrigoReis-dev@users.noreply.github.com>
2025-06-24 22:25:11 +1200

    Initial commit - added Main.java with basic print statements

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//  BIT504 Programming 2 - Assessment 2
+// Student Name: Caio Reis
+// Student ID: 513222
+// Assessment Number: 2
+
+public class Main {
+	public static void mai(String[] args) {
+		System.out.println("Hello!");
+		System.out.println("Task 9: Source and Version Control.");
+	}
+}
