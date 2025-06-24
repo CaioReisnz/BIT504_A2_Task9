6d686cad41d7f8f682606953a287d92c1a842ab2
Caio Reis <caioreis@myop.ac.nz>
2025-06-24 23:44:53 +1200

    Initial commit - added Main.java with basic print stateements

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@
 // Assessment Number: 2
 
 public class Main {
-	public static void mai(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Hello!");
 		System.out.println("Task 9: Source and Version Control.");
 	}
 }
+
+git config user.name "Caio Reis"
+git config user.email "caioreis@myop.ac.nz"
